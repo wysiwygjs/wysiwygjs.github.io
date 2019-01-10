54999125d819fff1a51dc64c1740cf630dfe94e9
Your Name <you@example.com>
2019-01-10 09:50:40 +0100

    Better API

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,5 +1,4 @@
 /*! wysiwyg.js | https://github.com/wysiwygjs/wysiwyg.js | Apache License (v2) */
-/*! Material Design Icons | https://material.io/ | Apache License (v2) */
 (function(window, document, undefined)
 {
     'use strict';
@@ -637,52 +636,12 @@
                 add_class( element, 'btn' );
                 if( 'icon' in button )
                 {
-                    // source: https://material.io/tools/icons/?icon=format_clear&style=outline
-                    var svg = null;
-                    switch( button.icon )
-                    {
-                        case 'link':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>';
-                            break;
-                        case 'bold':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M15.6 10.79c.97-.67 1.65-1.77 1.65-2.79 0-2.26-1.75-4-4-4H7v14h7.04c2.09 0 3.71-1.7 3.71-3.79 0-1.52-.86-2.82-2.15-3.42zM10 6.5h3c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5h-3v-3zm3.5 9H10v-3h3.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5z"/></svg>';
-                            break;
-                        case 'italic':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M10 4v3h2.21l-3.42 8H6v3h8v-3h-2.21l3.42-8H18V4h-8z"/></svg>';
-                            break;
-                        case 'underline':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 17c3.31 0 6-2.69 6-6V3h-2.5v8c0 1.93-1.57 3.5-3.5 3.5S8.5 12.93 8.5 11V3H6v8c0 3.31 2.69 6 6 6zm-7 2v2h14v-2H5z"/></svg>';
-                            break;
-                        case 'strikethrough':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M7.24 8.75c-.26-.48-.39-1.03-.39-1.67 0-.61.13-1.16.4-1.67.26-.5.63-.93 1.11-1.29.48-.35 1.05-.63 1.7-.83.66-.19 1.39-.29 2.18-.29.81 0 1.54.11 2.21.34.66.22 1.23.54 1.69.94.47.4.83.88 1.08 1.43s.38 1.15.38 1.81h-3.01c0-.31-.05-.59-.15-.85-.09-.27-.24-.49-.44-.68-.2-.19-.45-.33-.75-.44-.3-.1-.66-.16-1.06-.16-.39 0-.74.04-1.03.13s-.53.21-.72.36c-.19.16-.34.34-.44.55-.1.21-.15.43-.15.66 0 .48.25.88.74 1.21.38.25.77.48 1.41.7H7.39c-.05-.08-.11-.17-.15-.25zM21 12v-2H3v2h9.62c.18.07.4.14.55.2.37.17.66.34.87.51s.35.36.43.57c.07.2.11.43.11.69 0 .23-.05.45-.14.66-.09.2-.23.38-.42.53-.19.15-.42.26-.71.35-.29.08-.63.13-1.01.13-.43 0-.83-.04-1.18-.13s-.66-.23-.91-.42c-.25-.19-.45-.44-.59-.75s-.25-.76-.25-1.21H6.4c0 .55.08 1.13.24 1.58s.37.85.65 1.21c.28.35.6.66.98.92.37.26.78.48 1.22.65.44.17.9.3 1.38.39.48.08.96.13 1.44.13.8 0 1.53-.09 2.18-.28s1.21-.45 1.67-.79c.46-.34.82-.77 1.07-1.27s.38-1.07.38-1.71c0-.6-.1-1.14-.31-1.61-.05-.11-.11-.23-.17-.33H21V12z"/></svg>';
-                            break;
-                        case 'colortext':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path fill-opacity=".36" d="M0 20h24v4H0v-4z"/><path d="M11 3L5.5 17h2.25l1.12-3h6.25l1.12 3h2.25L13 3h-2zm-1.38 9L12 5.67 14.38 12H9.62z"/></svg>';
-                            break;
-                        case 'colorfill':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M16.56 8.94L7.62 0 6.21 1.41l2.38 2.38-5.15 5.15c-.59.59-.59 1.54 0 2.12l5.5 5.5c.29.29.68.44 1.06.44s.77-.15 1.06-.44l5.5-5.5c.59-.58.59-1.53 0-2.12zM5.21 10L10 5.21 14.79 10H5.21zM19 11.5s-2 2.17-2 3.5c0 1.1.9 2 2 2s2-.9 2-2c0-1.33-2-3.5-2-3.5z"/><path fill-opacity=".36" d="M0 20h24v4H0v-4z"/></svg>';
-                            break;
-                        case 'clearformat':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M20 8V5H6.39l3 3h1.83l-.55 1.28 2.09 2.1L14.21 8zM3.41 4.86L2 6.27l6.97 6.97L6.5 19h3l1.57-3.66L16.73 21l1.41-1.41z"/></svg>';
-                            break;
-                        case 'photo':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-4.86 8.86l-3 3.87L9 13.14 6 17h12l-3.86-5.14z"/></svg>';
-                            break;
-                        case 'attachment':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M16.5 6v11.5c0 2.21-1.79 4-4 4s-4-1.79-4-4V5c0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5v10.5c0 .55-.45 1-1 1s-1-.45-1-1V6H10v9.5c0 1.38 1.12 2.5 2.5 2.5s2.5-1.12 2.5-2.5V5c0-2.21-1.79-4-4-4S7 2.79 7 5v12.5c0 3.04 2.46 5.5 5.5 5.5s5.5-2.46 5.5-5.5V6h-1.5z"/></svg>';
-                            break;
-                        case 'emoticon':
-                            svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/></svg>';
-                            break;
-                    }
-                    if( svg )
-                    {
-                        var htmlparser = document.implementation.createHTMLDocument('');
-                        htmlparser.body.innerHTML = svg;
-                        element.appendChild( htmlparser.body.firstChild );
-                    }
+                    var htmlparser = document.implementation.createHTMLDocument('');
+                    htmlparser.body.innerHTML = button.icon;
+                    for( var child=htmlparser.body.firstChild; child !== null; child=child.nextSibling )
+                        element.appendChild( child );
                 }
-                if( button.attr )
+                if( 'attr' in button )
                 {
                     for( var key in button.attr )
                         element.setAttribute( key, button.attr[key] );
@@ -766,16 +725,16 @@
                                     open_popup_button( element, 'link', create_insertlink, recent_selection_link );
                                 break;
                             case 'bold':
-                                commands.bold().closePopup().collapseSelection()
+                                commands.bold().closePopup().collapseSelection();
                                 break;
                             case 'italic':
-                                commands.italic().closePopup().collapseSelection()
+                                commands.italic().closePopup().collapseSelection();
                                 break;
                             case 'underline':
-                                commands.underline().closePopup().collapseSelection()
+                                commands.underline().closePopup().collapseSelection();
                                 break;
                             case 'strikethrough':
-                                commands.strikethrough().closePopup().collapseSelection()
+                                commands.strikethrough().closePopup().collapseSelection();
                                 break;
                             case 'colortext':
                                 if( selection_rect )
@@ -788,6 +747,12 @@
                                     open_popup_selection( selection_rect, 'colorfill', create_colorpalette, false );
                                 else
                                     open_popup_button( element, 'colorfill', create_colorpalette, false );
+                                break;
+                            case 'subscript':
+                                commands.subscript().closePopup().collapseSelection();
+                                break;
+                            case 'superscript':
+                                commands.superscript().closePopup().collapseSelection();
                                 break;
                             case 'clearformat':
                                 commands.removeFormat().closePopup().collapseSelection();
