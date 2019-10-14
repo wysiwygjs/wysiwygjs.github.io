81f7f1f6fb2e3119915ff962a82c1c1e42584909
Your Name <you@example.com>
2019-10-14 14:47:17 +0200

    EXIF orientation

--- wysiwyg.js
+++ wysiwyg.js
@@ -489,6 +489,137 @@
             popup.appendChild( table );
         };
 
+        // read file as data-url
+        var filecontents = function( file, callback )
+        {
+            // base64 a 2GB video is insane: 16MB should work for an average image
+            if( file.size > 0x1000000 )
+                return;
+
+            // read file as data-url
+            var normalize_dataurl = function( orientation )
+            {
+                var filereader = new FileReader();
+                filereader.onload = function( e )
+                {
+                    if( ! orientation || orientation == 1 || orientation > 8 )
+                        return callback( file.type, e.target.result );
+                    // normalize
+                    var img = new Image();
+                    img.src = e.target.result;
+                    img.onload = function()
+                    {
+                        var width = img.width;
+                        var height = img.height;
+                        if( width > height )
+                        {
+                            var max_width = 4096;
+                            if( width > max_width )
+                            {
+                                height *= max_width / width;
+                                width = max_width;
+                            }
+                        }
+                        else
+                        {
+                            var max_height = 4096;
+                            if( height > max_height )
+                            {
+                                width *= max_height / height;
+                                height = max_height;
+                            }
+                        }
+                        var canvas = document.createElement( 'canvas' );
+                        var ctx = canvas.getContext( '2d' );
+                        canvas.width = width;
+                        canvas.height = height;
+                        ctx.save();
+                        if( orientation > 4 )
+                        {
+                            canvas.width = height;
+                            canvas.height = width;
+                        }
+                        switch( orientation )
+                        {
+                            case 2: ctx.translate( width, 0 );
+                                    ctx.scale( -1, 1 );
+                                    break;
+                            case 3: ctx.translate( width, height );
+                                    ctx.rotate( Math.PI );
+                                    break;
+                            case 4: ctx.translate( 0, height );
+                                    ctx.scale( 1, -1 );
+                                    break;
+                            case 5: ctx.rotate( 0.5 * Math.PI );
+                                    ctx.scale( 1, -1 );
+                                    break;
+                            case 6: ctx.rotate( 0.5 * Math.PI );
+                                    ctx.translate( 0, -height );
+                                    break;
+                            case 7: ctx.rotate( 0.5 * Math.PI );
+                                    ctx.translate( width, -height );
+                                    ctx.scale( -1, 1 );
+                                    break;
+                            case 8: ctx.rotate( -0.5 * Math.PI );
+                                    ctx.translate( -width, 0 );
+                                    break;
+                        }
+                        ctx.drawImage( img, 0, 0, width, height );
+                        ctx.restore();
+                        var dataURL = canvas.toDataURL( 'image/jpeg', 0.99 );
+                        callback( file.type, dataURL );
+                    }
+                };
+                filereader.readAsDataURL( file );
+            }
+            if( ! window.DataView )
+                return normalize_dataurl();
+
+            // get orientation - https://stackoverflow.com/questions/7584794/accessing-jpeg-exif-rotation-data-in-javascript-on-the-client-side
+            var filereader = new FileReader();
+            filereader.onload = function( e )
+            {
+                var contents = e.target.result;
+                var view = new DataView( contents );
+                // Not a JPEG at all?
+                if( view.getUint16(0, false) != 0xFFD8 )
+                    return normalize_dataurl();
+                var length = view.byteLength,
+                    offset = 2;
+                while( offset < length )
+                {
+                    // Missing EXIF?
+                    if( view.getUint16(offset+2, false) <= 8 )
+                        return normalize_dataurl();
+                    var marker = view.getUint16(offset, false);
+                    offset += 2;
+                    if( marker == 0xFFE1 )
+                    {
+                        if( view.getUint32(offset += 2, false) != 0x45786966 )
+                            return normalize_dataurl();
+                        var little = view.getUint16( offset += 6, false ) == 0x4949;
+                        offset += view.getUint32( offset + 4, little );
+                        var tags = view.getUint16( offset, little );
+                        offset += 2;
+                        for( var i=0; i < tags; ++i )
+                        {
+                            if( view.getUint16(offset + (i * 12), little) == 0x0112 )
+                            {
+                                var orientation = view.getUint16( offset + (i * 12) + 8, little );
+                                return normalize_dataurl( orientation );
+                            }
+                        }
+                    }
+                    else if( (marker & 0xFF00) != 0xFF00 )
+                        break;
+                    else
+                        offset += view.getUint16( offset, false );
+                }
+                return normalize_dataurl();
+            };
+            filereader.readAsArrayBuffer( file );
+        };
+
         // open popup and apply position
         var popup_position = function( popup, left, top ) // left+top relative to container
         {
@@ -658,16 +789,10 @@
                                         var file = files[i];
                                         if( 'browse' in button )
                                             button.browse( commands, input,file, element );
-                                        // base64 a 2GB video is insane
-                                        else if( file.size < 0x1000000 )
-                                        {
-                                            var filereader = new FileReader();
-                                            filereader.onloadend = function( e )
-                                            {
-                                                button.dataurl( commands, file.type,e.target.result, element );
-                                            };
-                                            filereader.readAsDataURL( file );
-                                        }
+                                        else
+                                            filecontents( file, function( type, dataurl ) {
+                                                button.dataurl( commands, type, dataurl, element );
+                                            });
                                     }
                                 }
                                 if( remove_input )
@@ -1337,14 +1462,9 @@
             if( ! item.type.match(/^image\//) )
                 return;
             // Insert image from clipboard
-            var filereader = new FileReader();
-            filereader.onloadend = function( e )
-            {
-                var image = e.target.result;
-                if( image )
-                    execCommand( 'insertImage', image );
-            };
-            filereader.readAsDataURL( item.getAsFile() );
+            filecontents( item.getAsFile(), function( type, dataurl ) {
+                execCommand( 'insertImage', dataurl );
+            });
             cancelEvent( e ); // dismiss paste
         });
 
